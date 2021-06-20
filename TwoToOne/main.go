ef739b5c61c7d823be67a9d0fce5792f1bae60b6
Antonio Thomacelli <tonnytg@gmail.com>
2021-06-19 22:55:27 -0300

    add twotoone

---
+++ TwoToOne/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+var (
+	a string = "xyaabbbccccdefww"
+	b string = "xxxxyyyyabklmopq"
+)
+
+func MySort(a, b string) string {
+	fmt.Println("Recebi a:", a)
+	fmt.Println("Recebi b:", b)
+
+	//d []string =
+	//
+	//for _, value := range
+
+
+	return "recebidos!"
+}
+
+func main()  {
+	fmt.Println(MySort(a, b))
+}
